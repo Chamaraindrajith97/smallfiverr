762c04bcb86358ea0d86affe3aebe6a318ac54b6
Chamara Indrajith <51538345+Chamaraindrajith97@users.noreply.github.com>
2020-11-03 01:42:18 +0530

    Create services.php

---
+++ mechazy/services.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="https://mechazy.com/assets/front/css/chamara-sandeep.css">
+    </head>
+    <!-- Modal script -->
+    <script>
+      $(function() {
+        $('.pop').on('click', function() {
+          $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+          $('#imagemodal').modal('show');   
+        });		
+    });
+    </script>
+        <!-- Modal script -->
+
+        <!-- Modal -->
+<div class="modal fade" id="imagemodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">              
+      <div class="modal-body">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <img src="" class="imagepreview" class="modal-image-width-eka" >
+      </div>
+    </div>
+  </div>
+  </div>
+  <!-- Modal -->
+        
+    <body class="background-body-service-details">
+        <div class="blue-box blue-box-title"><span class="title-2">Corepump (exerxise Machine)</span></div>
+        
+        
+        <div>
+        <p class="thani-nil-kotuwa">
+            dvsdn sdvjsdfvij sdvhijjsikfdv skjdhvsjkdfv sdjkvhlsikvdjhbsdv sdjkvhbloikvdjhbsdfv jkshdbvijsdfv kjsdfvbhkljsdfbv
+        </p><p>
+        </p>
+        </div>
+        
+        
+        <div class="images-hari-lakunu">
+        <br>&nbsp; &nbsp; &nbsp;<img src="https://mechazy.com/files/Green-tick.png" alt="Smiley face" height="18" width="18">&nbsp; Test<br>
+        <br>&nbsp; &nbsp; &nbsp;<img src="https://mechazy.com/files/Green-tick.png" alt="Smiley face" height="18" width="18">&nbsp; Test<br>
+        <br>&nbsp; &nbsp; &nbsp;<img src="https://mechazy.com/files/Green-tick.png" alt="Smiley face" height="18" width="18">&nbsp; Test<br>
+        <br>&nbsp; &nbsp; &nbsp;<img src="https://mechazy.com/files/Green-tick.png" alt="Smiley face" height="18" width="18">&nbsp; Test<br>
+        <br>&nbsp; &nbsp; &nbsp;<img src="https://mechazy.com/files/Green-tick.png" alt="Smiley face" height="18" width="18">&nbsp; Test<br>
+        <br>&nbsp; &nbsp; &nbsp;<img src="https://mechazy.com/files/Green-tick.png" alt="Smiley face" height="18" width="18">&nbsp; Test<br>
+        <br>&nbsp; &nbsp; &nbsp;<img src="https://mechazy.com/files/Green-tick.png" alt="Smiley face" height="18" width="18">&nbsp; Test<br>
+        </div>
+        
+        <br><div class="blue-box2 blue-box-title">sdvsdn sdvjsdfvij sdvhijjsikfdv skjdhvsjkdfv sdjkvhlsikvdjhbsdv sdjkvhbloikvdjhbsdfv jkshdbvijsdfv kjsdfvbhkljsdfbv</div> <br>
+        
+        <!-- RECENT PROJECTS -->
+        <section class="section">
+        <div class="trapezoid"><span class="title-2">RECENT PROJECTS</span></div>
+
+
+
+        <div class="lightblue-box">
+        <div class="blue-box"><span class="title-2">Project Name - Corepump (exerxise Machine)</span></div>
+
+        <div class="img-boxes">
+          <a class="pop">
+            <img src="https://mechazy.com/files/WhatsApp Image 2020-11-01 at 22.52.06.jpeg" class="images-class-eka">
+          </a>
+            
+          <a class="pop">
+            <img src="https://mechazy.com/files/WhatsApp Image 2020-11-01 at 22.52.06.jpeg" class="images-class-eka">
+          </a>
+          
+          <a class="pop">
+            <img src="https://mechazy.com/files/WhatsApp Image 2020-11-01 at 22.52.06.jpeg" class="images-class-eka">
+          </a>
+      </div>
+        
+        <div class="content1">(Click to See project details)</div>
+        <div type="button" class="collapsible"><div class="title-3">Project Details <i class="fas fa-chevron-down"></i></div></div>
+        <div class="content1">
+        <div>
+            <br>&nbsp; &nbsp; <b> Service Provided</b><br>
+            <div class="inside-container project-wisthara-list">
+                <br>Concept Development<br>
+                <br>Concept Development<br>
+                <br>Concept Development<br>
+                <br>Concept Development<br>
+                <br>Concept Development<br>
+                </div>
+        </div>
+            <br>&nbsp; &nbsp; <b> Total Budget - 500$</b><br>
+            <br>&nbsp; &nbsp; <b> Complete Project Duration - 30 Days</b><br>
+
+            <br>&nbsp; &nbsp; <b> Project In brief :</b><br>
+
+            <div>
+                <p class="inside-container">Lorem Ipsum is simply dummy text of the printing and
+                 typesetting industry. Lorem Ipsum has been the industry's 
+                 standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                   specimen book. It has survived not only five centuries, but
+                    also the leap into electronic typesetting, remaining essentially
+                     unchanged. It was popularised in the 1960s with the release of 
+                     Letraset sheets containing Lorem Ipsum passages, 
+                     and more recently with desktop publishing 
+                software like Aldus PageMaker including versions of Lorem Ipsum.
+                
+                <br><br>
+                Live Product - <a href="https://www.google.com" target="_blank">https://www.google.com</a>
+                <br><br>
+            </p>
+
+            </div>
+        </div>
+
+        </div>
+        </section>
+        <!-- RECENT PROJECTS -->
+
+
+
+<br><br>
+
+
+
+        <section class="section">
+          <!-- <div class="trapezoid"><span class="title-2">RECENT PROJECTS</span></div> -->
+  
+  
+  
+          <div class="lightblue-box">
+          <div class="blue-box"><span class="title-2">Project Name - Corepump (exerxise Machine)</span></div>
+  
+          <div class="img-boxes">
+            <a class="pop">
+              <img src="https://mechazy.com/files/WhatsApp Image 2020-11-01 at 22.52.06.jpeg" class="images-class-eka">
+            </a>
+              
+            <a class="pop">
+              <img src="https://mechazy.com/files/WhatsApp Image 2020-11-01 at 22.52.06.jpeg" class="images-class-eka">
+            </a>
+            
+            <a class="pop">
+              <img src="https://mechazy.com/files/WhatsApp Image 2020-11-01 at 22.52.06.jpeg" class="images-class-eka">
+            </a>
+        </div>
+          
+          <div class="content1">(Click to See project details)</div>
+          <div type="button" class="collapsible"><div class="title-3">Project Details <i class="fas fa-chevron-down"></i></div></div>
+          <div class="content1">
+          <div>
+              <br>&nbsp; &nbsp; <b> Service Provided</b><br>
+              <div class="inside-container project-wisthara-list">
+                  <br>Concept Development<br>
+                  <br>Concept Development<br>
+                  <br>Concept Development<br>
+                  <br>Concept Development<br>
+                  <br>Concept Development<br>
+                  </div>
+          </div>
+              <br>&nbsp; &nbsp; <b> Total Budget - 500$</b><br>
+              <br>&nbsp; &nbsp; <b> Complete Project Duration - 30 Days</b><br>
+  
+              <br>&nbsp; &nbsp; <b> Project In brief :</b><br>
+  
+              <div>
+                  <p class="inside-container">Lorem Ipsum is simply dummy text of the printing and
+                   typesetting industry. Lorem Ipsum has been the industry's 
+                   standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a type
+                     specimen book. It has survived not only five centuries, but
+                      also the leap into electronic typesetting, remaining essentially
+                       unchanged. It was popularised in the 1960s with the release of 
+                       Letraset sheets containing Lorem Ipsum passages, 
+                       and more recently with desktop publishing 
+                  software like Aldus PageMaker including versions of Lorem Ipsum.
+                  
+                  <br><br>
+              Live Product - <a href="https://www.google.com" target="_blank">https://www.google.com</a>
+                  <br><br>
+              </p>
+  
+              </div>
+          </div>
+  
+          </div>
+          </section>
+          <!-- RECENT PROJECTS -->
+  
+
+
+
+
+          <br><br>
+
+
+
+          <section class="section">
+            <!-- <div class="trapezoid"><span class="title-2">RECENT PROJECTS</span></div> -->
+    
+    
+    
+            <div class="lightblue-box">
+            <div class="blue-box"><span class="title-2">Project Name - Corepump (exerxise Machine)</span></div>
+    
+            <div class="img-boxes">
+                <a class="pop">
+                  <img src="https://mechazy.com/files/WhatsApp Image 2020-11-01 at 22.52.06.jpeg" class="images-class-eka">
+                </a>
+                  
+                <a class="pop">
+                  <img src="https://mechazy.com/files/WhatsApp Image 2020-11-01 at 22.52.06.jpeg" class="images-class-eka">
+                </a>
+                
+                <a class="pop">
+                  <img src="https://mechazy.com/files/WhatsApp Image 2020-11-01 at 22.52.06.jpeg" class="images-class-eka">
+                </a>
+            </div>
+            
+            <div class="content1">(Click to See project details)</div>
+            <div type="button" class="collapsible"><div class="title-3">Project Details <i class="fas fa-chevron-down"></i></div></div>
+            <div class="content1">
+            <div>
+                <br>&nbsp; &nbsp; <b> Service Provided</b><br>
+                <div class="inside-container project-wisthara-list">
+                    <br>Concept Development<br>
+                    <br>Concept Development<br>
+                    <br>Concept Development<br>
+                    <br>Concept Development<br>
+                    <br>Concept Development<br>
+                    </div>
+            </div>
+                <br>&nbsp; &nbsp; <b> Total Budget - 500$</b><br>
+                <br>&nbsp; &nbsp; <b> Complete Project Duration - 30 Days</b><br>
+    
+                <br>&nbsp; &nbsp; <b> Project In brief :</b><br>
+    
+                <div>
+                    <p class="inside-container">Lorem Ipsum is simply dummy text of the printing and
+                     typesetting industry. Lorem Ipsum has been the industry's 
+                     standard dummy text ever since the 1500s, when an unknown
+                      printer took a galley of type and scrambled it to make a type
+                       specimen book. It has survived not only five centuries, but
+                        also the leap into electronic typesetting, remaining essentially
+                         unchanged. It was popularised in the 1960s with the release of 
+                         Letraset sheets containing Lorem Ipsum passages, 
+                         and more recently with desktop publishing 
+                    software like Aldus PageMaker including versions of Lorem Ipsum.
+                    
+                    <br><br>
+                    Live Product - <a href="https://www.google.com" target="_blank">https://www.google.com</a>
+                    <br><br>
+                </p>
+    
+                </div>
+            </div>
+    
+            </div>
+            </section>
+            <!-- RECENT PROJECTS -->
+    
+  
+
+
+        
+            
+
+        </body>
+
+        <script>
+            var coll = document.getElementsByClassName("collapsible");
+            var i;
+            
+            for (i = 0; i < coll.length; i++) {
+              coll[i].addEventListener("click", function() {
+                this.classList.toggle("active1");
+                var content1 = this.nextElementSibling;
+                if (content1.style.display === "block") {
+                  content1.style.display = "none";
+                } else {
+                  content1.style.display = "block";
+                }
+              });
+            }
+            </script>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
